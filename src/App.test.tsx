ded8e8a65e4ef60981f11e61122ee635b60d66eb
Danh <handoiditu1508@gmail.com>
2022-06-11 10:29:20 +0700

    reformat redux folder structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { store } from './redux/app/store';
 
 test('renders learn react link', () => {
   const { getByText } = render(
